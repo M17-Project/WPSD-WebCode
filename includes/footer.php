ed65bb90be0be7834252e59aef95f45912720b6a
DK1MI <dk1mi@qrz.is>
2025-07-22 07:32:20 +0200

    added link to wiki in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
         <div class="footer">
            <?php
-                echo 'Get WPSD (M17 Edition) Help: <a href="https://m17project.org/contact/" target="_new">M17project.org</a><br><br>';
+                echo 'Get WPSD (M17 Edition) Help: <a href="https://m17project.org/contact/" target="_new">M17project.org</a> -- <a href="https://wiki.m17foundation.org/index.php?title=WPSD_M17_Community_Fork" target="_new">PLEASE READ OUR WIKI PAGE FOR KNOWN BUGS AND FAQS!</a><br><br>';
                 echo '<a href="https://wpsd.radio/">WPSD</a> Made in Winona, Minn. USA by Chip Cuccio, <code>W0CHP</code> &copy; 2020-'.date("Y").' -- WPSD Project <a href="https://w0chp.radio/wpsd/#credits" target="_new">Credits</a>';
            ?>
         </div>
